b80ce3c74c7c2f8decdc795bce1cebddb355d5e5
Kmiz07 <kmiz07@gmail.com>
2023-11-13 02:44:13 +0100

    Add files via upload

--- test_servsock.py
+++ test_servsock.py
@@ -2,11 +2,13 @@ import uasyncio
 import network
 import servsock
 from machine import Pin
-red = 'xxxxxxxxxx'
+red = 'xxxxxxxxxxx'
 clave = 'xxxxxxxxxx'
-actualiza=False
 mensaje_completo = b''
 estado_led=1
+flash=Pin(0,Pin.IN)
+led=Pin(2,Pin.OUT)
+led.value(1)
 def conecta_wifi():
             wifi = network.WLAN(network.STA_IF)
             wifi.active(True)
@@ -24,9 +26,8 @@ def conecta_wifi():
             print('************************+')
 conecta_wifi()
         
-def si_recibe(objeto,cliente,mensaje):
+async def si_recibe(objeto,cliente,mensaje):
     global estado_led
-    global actualiza
     #objeto= para acceder a todas las funciones del objeto ejemplo: objeto.finaliza()
     #cliente= lista con datos del cliente[socket,IP,puerto,¿es winsocket?(normalmente sera True)]
     #mensaje=mensaje recibido
@@ -36,25 +37,24 @@ def si_recibe(objeto,cliente,mensaje):
             estado_led=1
         else:
             estado_led=0
-    if mensaje == 'WS Conectado':
-        actualiza=True
-
+    if mensaje== 'WS Conectado':
+        await objeto.envio_a_clientes(f'Flash={flash.value()}')
+        await objeto.envio_a_clientes(f'led={led.value()}')
 async def inicio():
     global tamaño_bufer
     global estado_led
-    global actualiza
+    estado_flash=1
     flash=Pin(0,Pin.IN)
     estado_flash=1
     led=Pin(2,Pin.OUT)
     led.value(1)
     servidor=servsock.Servsock(si_recibe,80,200,False)#funcion de retorno,puerto,tamaño bufer(-1, todo el mensaje),mensajes encriptados(si se encriptan los mensajes chrome no funcionara correctamente)
     while True:
-        if flash.value() != estado_flash or actualiza:
+        if flash.value() != estado_flash:
             await servidor.envio_a_clientes(f'Flash={flash.value()}')
             estado_flash=flash.value()
             await uasyncio.sleep(0)
-        if led.value() != estado_led or actualiza:
-            actualiza= False
+        if led.value() != estado_led:
             led.value(estado_led)
             await servidor.envio_a_clientes(f'led={led.value()}')
         await uasyncio.sleep(0)
